4c437448fc76e2b6cfdb4cde1284bcbc2eec58ef
Tai <tantai.it95@gmail.com>
2022-03-20 12:27:48 +0700

    add head to all page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Head from "next/head";
 
 const supportedChainIds = [4];
 const connector = {
@@ -13,6 +14,15 @@ function MyApp({ Component, pageProps }) {
       supportedChainIds={supportedChainIds}
       connectors={connector}
     >
+      <Head>
+        <title>CoinOne</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/logo.png" />
+        <meta
+          name="facebook-domain-verification"
+          content="2texl7ozql70o8y075dpem07rw24ns"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThirdwebWeb3Provider>
   );
